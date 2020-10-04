ad09c72282e3fb8c6591575ec7ab05d4f2e445a6
Michael Greenwald <mick,gr@gmail.com>
2020-10-04 23:27:20 +0300

    fix configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,24 @@
+import React from "react";
+import { IntlProvider } from "react-intl";
+import GlobalStyles from "styles/globalStyles/globalStyles";
+import { DEFAULT_LOCALE, getMessages } from "res/i18n/i18n";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-}
+};
+
+const locale = DEFAULT_LOCALE;
+export const decorators = [
+  (Story) => (
+    <div>
+      <GlobalStyles />
+      <IntlProvider
+        locale={locale}
+        key={locale}
+        messages={getMessages(locale)}
+        textComponent={"span"}>
+        <Story />
+      </IntlProvider>
+    </div>
+  ),
+];
