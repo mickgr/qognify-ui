1894ed9119e161004634ea1eb7e5cb2ae58e1f25
Michael Greenwald <mick,gr@gmail.com>
2020-10-05 21:38:09 +0300

    Adjust project

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { IntlProvider } from "react-intl";
 import GlobalStyles from "styles/globalStyles/globalStyles";
-import { DEFAULT_LOCALE, getMessages } from "res/i18n/i18n";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -12,13 +10,7 @@ export const decorators = [
   (Story) => (
     <div>
       <GlobalStyles />
-      <IntlProvider
-        locale={locale}
-        key={locale}
-        messages={getMessages(locale)}
-        textComponent={"span"}>
-        <Story />
-      </IntlProvider>
+      <Story />
     </div>
   ),
 ];
--- src/res/i18n/i18n.ts
+++
@@ -1,5 +0,0 @@
-import translations from './locales';
-
-export const DEFAULT_LOCALE = 'en';
-
-export const getMessages = (locale: string): {} => translations[locale] || translations[DEFAULT_LOCALE];
