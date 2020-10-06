a9650af26a8906f88886a750807e54b503199e49
Michael Greenwald <mick,gr@gmail.com>
2020-10-06 19:17:52 +0300

    fic radio button tests

--- src/components/FormElements/CustomRadioButton/tests/CustomRadioButton.test.tsx
+++ src/components/FormElements/CustomRadioButton/tests/CustomRadioButton.test.tsx
@@ -1,55 +1,54 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import renderer from 'react-test-renderer';
-import 'jest-styled-components';
+import { mount, shallow } from "enzyme";
+import React from "react";
+import { FormattedMessage } from "react-intl";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
 
-import CustomRadioButton, { Props } from '../CustomRadioButton';
-import { StyledCustomRadioButtonLabel, StyledCustomRadioButton } from '../CustomRadioButtonStyles';
+import CustomRadioButton, { Props } from "../CustomRadioButton";
+import {
+  StyledCustomRadioButtonLabel,
+  StyledCustomRadioButton,
+} from "../CustomRadioButtonStyles";
 
-import styles from 'styles/values'
+import styles from "styles/values";
 
-describe('Custom radio button test', () => {
-	let props: Props;
-	beforeEach(() => {
-		props = {
-			labelText: <FormattedMessage id={'loading'} />,
-			disabled: undefined,
-			value: 'test value',
-			name: 'test custom button'
-		};
-	});
+describe("Custom radio button test", () => {
+  let props: Props;
+  beforeEach(() => {
+    props = {
+      labelText: "Option 1",
+      value: "option1",
+      name: "options",
+    };
+  });
 
-	it('should render custom radio button', () => {
-		const wrapper = shallow(<CustomRadioButton {...props} />);
-		expect(wrapper.exists()).toBe(true);
-	});
+  it("should render custom radio button ok", () => {
+    const wrapper = shallow(<CustomRadioButton {...props} />);
+    expect(wrapper.exists()).toBe(true);
+  });
 
-	it('should render disabled custom radio button', () => {
-		const wrapper = shallow(<CustomRadioButton {...props} disabled={true} />);
-		expect(wrapper.exists()).toBe(true);
-	});
+  it("should render custom radio button with proper props and sub elements", () => {
+    const wrapper = mount(<CustomRadioButton {...props} />);
+    const input = wrapper.find("input");
+    expect(input.exists()).toBe(true);
+    const label = wrapper.find("label");
+    expect(label.exists()).toBe(true);
+    expect((input.props() as any).type).toEqual("radio");
+    expect((input.props() as any).id).toEqual(props.name + props.value);
+    expect((input.props() as any).name).toEqual(props.name);
+    expect((input.props() as any).value).toEqual(props.value);
 
-	it('should render StyledCustomRadioButtonLabel', () => {
-		const wrapper = renderer.create(<StyledCustomRadioButtonLabel />).toJSON();
-		expect(wrapper).toHaveStyleRule('color', `${styles.color.shade.DARK06}`);
-		expect(wrapper).toHaveStyleRule('pointer-events', 'auto');
-	});
+    expect(label).toHaveStyleRule("color", `${styles.color.shade.DARK06}`);
+    expect(label).toHaveStyleRule("pointer-events", "auto");
+  });
 
-	it('should render disabled StyledCustomRadioButtonLabel', () => {
-		const wrapper = renderer.create(<StyledCustomRadioButtonLabel disabled={true} />).toJSON();
-		expect(wrapper).toHaveStyleRule('color', `${styles.color.shade.DARK04}`);
-		expect(wrapper).toHaveStyleRule('pointer-events', 'none');
-	});
+  it("should render disabled custom radio button", () => {
+    const wrapper = mount(<CustomRadioButton {...props} disabled={true} />);
+    const input = wrapper.find("input");
+    const label = wrapper.find("label");
+    expect((input.props() as any).disabled).toEqual(true);
 
-	it('should render StyledCustomRadioButton', () => {
-		const wrapper = renderer.create(<StyledCustomRadioButton />).toJSON();
-		expect(wrapper).toHaveStyleRule('border', `5px solid ${styles.color.brand.PRIMARY}`, {modifier: ':checked + label:before'});
-	});
-
-	it('should render disabled StyledCustomRadioButton', () => {
-		const wrapper = renderer.create(<StyledCustomRadioButton disabled={true} />).toJSON();
-		expect(wrapper).toHaveStyleRule('border', `5px solid ${styles.color.shade.LIGHT}`, {modifier: ':checked + label:before'});
-	});
-
+    expect(label).toHaveStyleRule("color", `${styles.color.shade.DARK04}`);
+    expect(label).toHaveStyleRule("pointer-events", "none");
+  });
 });
