636f58aa8d6f48627cce041ed039e58d1e2d92a1
Michael Greenwald <mick,gr@gmail.com>
2020-10-08 19:31:36 +0300

    Adjust to relative imports

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "@storybook/addon-console";
-import GlobalStyles from "styles/globalStyles/globalStyles";
+import GlobalStyles from "../src/styles/globalStyles/globalStyles";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
