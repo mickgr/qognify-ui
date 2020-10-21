18c50126bd8d16cc7f93f6d1bcc51d67b1c63bc3
Michael Greenwald <mick,gr@gmail.com>
2020-10-21 22:19:38 +0300

    convert to rollup

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import image from "@rollup/plugin-image";
+
+import packageJson from "./package.json";
+const input = "./src/index.ts";
+
+export default [
+  // CommonJS
+  {
+    input,
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ useTsconfigDeclarationDir: true }),
+      image(),
+    ],
+  },
+];
